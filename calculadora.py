4c3d56759e63f594dec75f1dda524b7a2a72f877
anthonny-420 <2808anthonny@gmail.com>
2024-04-19 17:25:22 -0500

    deje la calculadora tal cual estaba

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,11 @@
 # Vamos a crear mi primera calculadora!!!!
 mesa = input("¿Hola, que mesa reservó?: ",)
-mesa =int(mesa) 
+mesa = int(mesa)
 
 consumo = input("¿cuanto fue su consumo total?: ")
-propina = input("¿teniendo en cuenta que la propina es voluntaria y la atencion recibida, cuanto desea dar de propina en modo de porcentaje? ")
+propina = input(
+    "¿teniendo en cuenta que la propina es voluntaria y la atencion recibida, cuanto desea dar de propina en modo de porcentaje? ")
 
 consumo_total = float(consumo) * float(propina) / 100
-resultado_operacion =print(f"considerando que el monto total fue de: {consumo}, su propina equivale a {consumo_total}")
-
-
-
+resultado_operacion = print(
+    f"considerando que el monto total fue de: {consumo}, su propina equivale a {consumo_total}")
