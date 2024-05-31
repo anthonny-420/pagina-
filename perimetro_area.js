7567a41bb6f9435746eb67e2fe257a858c973205
Anthonny Barrera <2808anthonny@gmail.com>
2024-05-31 13:31:33 -0500

    programa que encuentra el perimetro de un rectangulo

---
+++ perimetro_area.js
@@ -0,0 +1,18 @@
+dato_1 = 17
+dato_2 = 84
+dato_3 = 17
+dato_4 = 84
+
+function area_cuadrado(dato_1, dato_2) {
+    let area = dato_1 * dato_2;
+    console.log(`Teniendo en cuenta los datos proporcionados, el area equivale a: ${area} `)
+}
+
+area_cuadrado(dato_1, dato_2)
+
+function perimetro_cuadrado(dato_1, dato_2, dato_3, dato_4) {
+    let perimetro = dato_1 + dato_2 + dato_3 + dato_4;
+    console.log(`Teniendo en cuenta los datos proporcionados, el perimetro equivale a: ${perimetro}`)
+}
+
+perimetro_cuadrado(dato_1, dato_2, dato_3, dato_4)
